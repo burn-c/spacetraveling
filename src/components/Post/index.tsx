7e4fc55272f8065c54283c46ddde753cf2c6b2b7
Carlos Oliveira <carlos.burndev@gmail.com>
2023-09-28 21:03:06 -0300

    style: adds on Post and Posts page

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,22 +1,26 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import styles from './post.module.scss';
 
-export default function Post({ title, description, date, author }) {
+export default function Post({ title, description, date, author, id }) {
   return (
     <div className={styles.postContainer}>
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <a href={`/post/${id}`}>
+        <h1>{title}</h1>
+        <p>{description}</p>
 
-      <div className={styles.infoContainer}>
-        <div>
-          <FiCalendar color="#BBBBBB" size="20px" /> {date}
-        </div>
+        <div className={styles.infoContainer}>
+          <div>
+            <FiCalendar size="20px" /> {date}
+          </div>
 
-        <div>
-          <FiUser color="#BBBBBB" size="20px" /> {author}
+          <div>
+            <FiUser size="20px" /> {author}
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 }
